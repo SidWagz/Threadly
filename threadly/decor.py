d4390abad9a8f8617a3a2857bf1e7a0fc5d0aa5f
Sidwagz <siddharthwagle1@gmail.com>
2016-05-21 22:09:39 +0530

    * POST requests template added, can have GET and POST template names will be used on the request sent to server. * Removed '/' from main Blueprint, bad routing choice.

--- threadly/decor.py
+++ threadly/decor.py
@@ -20,3 +20,37 @@ def templated(template=None):
 
         return decorated_function
     return decorator
+
+def two_way_templated(get_template=None, post_template=None):
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            get_template_name = get_template
+            post_template_name = post_template
+
+            ctx = f(*args, **kwargs)
+            print(f.__name__)
+            print(args, kwargs)
+            if ctx is None:
+                ctx = {}
+            elif not isinstance(ctx, dict):
+                return ctx
+
+            if request.method == 'GET':
+                print(request.method)
+                print(get_template_name)
+                if get_template_name is None:
+                    return ctx
+                else:
+                    return render_template(get_template_name, **ctx)
+
+            if request.method == 'POST':
+                print(request.method)
+                print(post_template_name)
+                if post_template_name is None:
+                    return ctx
+                else:
+                    return render_template(post_template_name, **ctx)
+
+        return decorated_function
+    return decorator
--- threadly/route.py
+++ threadly/route.py
@@ -1,15 +1,35 @@
 
-from flask import Flask, Blueprint, request, render_template, url_for
+from flask import Flask, Blueprint, request, render_template, url_for, jsonify
 from threadly.decor import templated
 from threadly import app
-
 import os
+
 HTML_TEMPLATES = '/'.join((os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'html'))
 
-threadly = Blueprint('threadly', __name__, url_prefix='/',
+threadly = Blueprint('threadly', __name__,
                      template_folder=HTML_TEMPLATES)
 
-@threadly.route('/')
+@threadly.route('/', methods=['GET'])
 @templated('home.html')
 def home():
-    return {}
+    if request.method == 'GET':
+
+        # @DEBUG : debug all routes registered
+        # links = []
+        # for rule in app.url_map.iter_rules():
+        #     links.append(rule.rule)
+        #
+        # print('-----------------------')
+        # for link in links:
+        #     print(link)
+        # print('-----------------------')
+
+        return {}
+
+@threadly.route('/_post_comment', methods=['POST'])
+def post_comment():
+    if request.method == 'POST':
+
+        values = {}
+
+        return jsonify(**values)
